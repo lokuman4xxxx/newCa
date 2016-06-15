055ba67130aee9e14b6cc512fd7cb2a115e58179
Ruicheng Shi <8r0ltt@gmail.com>
2016-06-15 22:21:01 +0800

    carousel temp see wiznote

--- src/js/all.js
+++ src/js/all.js
@@ -1,33 +1,95 @@
 // Homepage Carousel
+
+$(document).ready(function () {
+	var sliderNum = $(".item").length;
+	var cloneBoxA = [];
+	var cloneBoxP = [];
+	var carouselComponentWidth = $("#carousel").innerWidth();
+	var carouselViewInit = (function () {
+		for (var i = 0; i < sliderNum - 1; i++) {
+			cloneBoxA[i] = $(".item").eq(i)
+				.clone()
+				.addClass("itemCloned");
+		}
+		for (var i = sliderNum - 1; i > 0; i--) {
+			cloneBoxP[i] = $(".item").eq(i)
+				.clone()
+				.addClass("itemCloned");
+		}
+		$("#carouselView").prepend(cloneBoxP);
+		$("#carouselView").append(cloneBoxA);
+
+		sliderNum = $(".item").length;
+		carouselComponentWidth = carouselComponentWidth * 0.6;
+		var carouselBoxWidth = carouselComponentWidth * sliderNum;
+		var translateWidth = carouselComponentWidth * 2.7;
+
+		$("#carouselView").css({
+			"width": carouselBoxWidth + "px",
+			"transform": "translateX(-" + translateWidth + "px)",
+		});
+
+		$(".item").css({
+			"width": carouselComponentWidth + "px"
+		});
+
+	} ());
+
+	var defaultX = carouselComponentWidth * 3.7;
+
+});
+
+var carouselProp = {
+	counter: 0,
+	times: 1,
+	counterWidth: -534,
+	defaultWidth: -1441.8
+};
+
 Vue.component('carousel', {
 	template: '#carouselComponent',
-
-
-
-	methods: {
-		prev: function (event) {
-			event.preventDefault();
-			console.log("prev success");
-
-		},
-		next: function (event) {
-			event.preventDefault();
-			console.log("next success");
-		}
-
-	}
 });
 
 new Vue({
-	el: '#carousel'
-});
+	el: '#carousel',
 
+	methods: {
+		prev: function (e) {
+			e.preventDefault();
 
-$(document).ready(function () {
-	var carouselView = (function () {
-		var carouselLength = $("#carouselView").find(".item").css("height");
-		console.log(carouselLength);
-			// $("#carouselView").css("")
-	}());
+			if (carouselProp.counter === 0) {
+				carouselProp.defaultWidth = -1441.8;
+				carouselProp.times = 1;
+				$("#carouselView").css({
+					"transform": "translateX(" + (carouselProp.defaultWidth - (carouselProp.counterWidth * carouselProp.times)) + "px)",
+					"transition": ".6s all ease-in",
+				});
 
+				setTimeout(function () {
+					$("#carouselView").css({
+						"transform": "translateX(-3043.8px)",
+						"transition": "none"
+					});
+					carouselProp.counter = 3;
+				}, 700);
+			} else {
+				carouselProp.defaultWidth = -3043.8;
+				$("#carouselView").css({
+					"transform": "translateX(" + (carouselProp.defaultWidth - (carouselProp.counterWidth * carouselProp.times)) + "px)",
+					"transition": ".6s all ease-in",
+				});
+				carouselProp.counter--;
+				carouselProp.times++;
+
+			}
+		},
+		next: function (e) {
+			e.preventDefault();
+			$("#carouselView").css({
+				"transform": "translateX(-" + (defaultX + (carouselComponentWidth * carouselProp.counter)) + "px)",
+				"transition": ".6s all ease-in",
+			});
+			counter--;
+		},
+	}
 });
